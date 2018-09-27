f9c01478b7763ad1163328637d36de42701f7e5d
Saket <sk846694@gmail.com>
2018-09-27 11:27:00 +0530

    navigation drawer for admin added

--- app/src/main/java/com/example/mnnit/mnnitdean/adminActivity.java
+++ app/src/main/java/com/example/mnnit/mnnitdean/adminActivity.java
@@ -1,11 +1,17 @@
 package com.example.mnnit.mnnitdean;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,11 +26,46 @@ public class adminActivity extends AppCompatActivity {
     user prin;
     ArrayList<user> arrayList;
     RecyclerView recyclerView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        auth=FirebaseAuth.getInstance();
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.admindrawer);
+        NavigationView navigationView=findViewById(R.id.nav_admin);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+                int id=menuItem.getItemId();
+                switch (id){
+                    case R.id.notice:break;
+                    case R.id.up_notice:
+                        Intent intent=new Intent(adminActivity.this,upnotice.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.calendar:break;
+                    case R.id.downloads:break;
+                    case R.id.fee:break;
+                    case R.id.signout:
+                        auth.signOut();
+                        finish();
+                        Intent i=new Intent(adminActivity.this,loginType.class);
+                        startActivity(i);
+                        break;
+                }
+
+                return true;
+            }
+        });
+        mToggle=new ActionBarDrawerToggle(adminActivity.this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         arrayList=new ArrayList<user>();
         recyclerView=(RecyclerView)findViewById(R.id.recycler);
@@ -50,5 +91,13 @@ public class adminActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        if(mToggle.onOptionsItemSelected(menuItem)){
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
